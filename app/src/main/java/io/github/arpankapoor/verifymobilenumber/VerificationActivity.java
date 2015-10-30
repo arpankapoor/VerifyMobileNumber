bd5f7bb850e9d9423132f4c694f6c7c294c92b40
Arpan Kapoor <k.arpan26@gmail.com>
2015-10-30 17:19:05 +0530

    Add SMS receiver BroadcastReceiver

--- app/src/main/java/io/github/arpankapoor/verifymobilenumber/VerificationActivity.java
+++ app/src/main/java/io/github/arpankapoor/verifymobilenumber/VerificationActivity.java
@@ -1,14 +1,66 @@
 package io.github.arpankapoor.verifymobilenumber;
 
-import android.os.AsyncTask;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.provider.Telephony;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.telephony.SmsMessage;
+import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class VerificationActivity extends AppCompatActivity {
+
+    private BroadcastReceiver smsReceiver;
+
+    private void setStatus(boolean isVerified) {
+        String statusText = getResources().getString(R.string.verification_done);
+        int checkBoxVisibility = View.VISIBLE;
+        int progressBarVisibility = View.INVISIBLE;
+
+        if (!isVerified) {
+            statusText = getResources().getString(R.string.verifying);
+            checkBoxVisibility = View.INVISIBLE;
+            progressBarVisibility = View.VISIBLE;
+        }
+
+        TextView statusTextView = (TextView) findViewById(R.id.statusTextView);
+        ProgressBar progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        CheckBox checkBox = (CheckBox) findViewById(R.id.checkBox);
+
+        statusTextView.setText(statusText);
+        checkBox.setVisibility(checkBoxVisibility);
+        progressBar.setVisibility(progressBarVisibility);
+    }
+
+    /**
+     * Listen for incoming SMS
+     * https://developer.android.com/reference/android/provider/Telephony.Sms.Intents.html#SMS_RECEIVED_ACTION
+     */
+    private void registerSmsReceiver() {
+        this.smsReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                SmsMessage[] smsMessages = Telephony.Sms.Intents.getMessagesFromIntent(intent);
+                for (SmsMessage smsMessage : smsMessages) {
+                    String originatingAddress = smsMessage.getOriginatingAddress();
+                    String messageBody = smsMessage.getMessageBody();
+                    Log.v("origin", originatingAddress);
+                    Log.v("msg", messageBody);
+                }
+            }
+        };
+
+        registerReceiver(smsReceiver,
+                new IntentFilter(Telephony.Sms.Intents.SMS_RECEIVED_ACTION));
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,8 +70,18 @@ public class VerificationActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         boolean isVerified = getIntent().getBooleanExtra("is_verified", false);
+        String phoneNumber = getIntent().getStringExtra("phone_number");
+        Log.v("ph", phoneNumber);
+
+        setStatus(isVerified);
         if (!isVerified) {
-            // TODO: Send and receive verification SMS
+            registerSmsReceiver();
         }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(smsReceiver);
     }
 }
